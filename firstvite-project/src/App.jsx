33a5d17b3702fc7d39112051efc19956778cca42
pankaj <pkp20022@gmail.com>
2023-10-05 10:02:32 +0530

    adding-new-files

---
+++ firstvite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Chai from "./chai.jsx"
+
+
+function App() {
+
+  return (
+    <> 
+    {/* fregment */}
+    <Chai/>
+    <h3>hi this is pankaj</h3>
+    </>
+  )
+}
+
+export default App
